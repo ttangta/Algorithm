1923fbb1e2f40efba8be529a6e05db3478f4b627
ttangta <kimjh6836@gmail.com>
2024-09-27 17:52:01 +0900

    [Bronze V] Title: 구구단, Time: 188 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// 조건 1 <= N <= 9
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = 1 ; i<=9 ; i++) {
+			System.out.println(N + " * " + i + " = " + N*i);
+		}
+	}
+}
