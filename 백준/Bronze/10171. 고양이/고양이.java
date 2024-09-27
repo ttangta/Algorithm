55cf0337dc437d4dd404e3dfbc92144ad5d91041
ttangta <kimjh6836@gmail.com>
2024-09-27 18:42:14 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13892 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,6 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		// 일반적으로 출력이 되지않는 문자열을 이스케이프를 통해 출력
+		// => 일반적으로 출력되지 않는 문자가 존재할 경우 해결하는 방법을 위한 문제
 		System.out.println("\\    /\\");
 		System.out.println(" )  ( ')");
 		System.out.println("(  /  )");
