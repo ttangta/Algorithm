85cfb315aacf1a6b875b7fabe0a235dd137db9bf
ttangta <kimjh6836@gmail.com>
2024-09-27 17:26:00 +0900

    [Bronze V] Title: A×B, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	// 0 < A,B < 10 => int형 사용
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A * B);
+	}
+}
