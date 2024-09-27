0238abbbcea025f88f06015640057e2fd7d54e34
ttangta <kimjh6836@gmail.com>
2024-09-27 18:40:43 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		// 일반적으로 출력이 되지않는 문자열을 이스케이프를 통해 출력
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
