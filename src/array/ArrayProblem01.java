5b056369865dbc12d26e0ca04324e42103a9239f
ttangta <kimjh6836@namver.com>
2026-01-22 15:59:52 +0900

    프로그레머스 level.1 두 개 뽑아서 더하기

---
+++ src/array/ArrayProblem01.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// 프로그래머스 : 두 개 뽑아서 더하기
+public class ArrayProblem01 {
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(s3(new int[]{2, 1, 3, 4, 1})));
+        System.out.println(Arrays.toString(s3(new int[]{5, 0, 2, 7})));
+    }
+    public static int[] s3(int[] arr){
+        ArrayList<Integer> sumNumbers = new ArrayList<Integer>();
+        for(int i = 0 ; i < arr.length-1 ; i ++){
+            for(int j = i+1 ; j < arr.length ; j++){
+                // j반복문이 도는 동안 매번 arr[j]를 더할 arr[i] 값 초기화
+                int sum = arr[i];
+
+                // arr[j]를 더함
+                sumNumbers.add(sum+arr[j]);
+            }
+        }
+        return sumNumbers.stream().distinct().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
