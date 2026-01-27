754c65b74549a9196c737d817bf5ed54a4417d25
ttangta <kimjh6836@namver.com>
2026-01-27 15:24:41 +0900

    올바른 괄호_반복문 종료 후 Stack.isEmpty() 결과 반환

--- src/stack/CustomProblem01.java
+++ src/stack/CustomProblem01.java
@@ -13,13 +13,17 @@ public class CustomProblem01 {
 
         // 매개변수로 전달받은 문자열의 길이만큼 반복문 수행 O(N)
         for(int i=0; i<str.length(); i++){
+            // 만약 str의 i번째 문자가 "열린 괄호"인 경우 스택에 push
             if(str.charAt(i) == '(')stack.push(str.charAt(i));
+            // 만약 str의 i번째 문자가 "닫힌 괄호" 이면서
             else{
+                // 스택에 데이터가 존재하지 않을 경우는 올바른 괄호가 아님으로 바로 false 리턴
                 if(stack.isEmpty())return false;
+                // 스택에 데이터가 존재한다면 스택에 존재하는 가장 마지막 데이터 하나 pop
                 else stack.pop();
             }
         }
-        if(!stack.isEmpty())return false;
-        return true;
+
+        return stack.isEmpty();
     }
 }
