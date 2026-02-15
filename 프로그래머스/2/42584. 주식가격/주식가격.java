a4a30211d0bc2af70271a80c12400da92330ecd0
ttangta <kimjh6836@gmail.com>
2026-02-15 23:38:52 +0900

    [level 2] Title: 주식가격, Time: 66.89 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(0);
+
+        for(int i=1; i<prices.length; i++){
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+                int j = stack.pop();
+                answer[j] = i - j;
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            int i = stack.pop();
+            answer[i] = prices.length - 1 - i;
+        }
+
+
+        return answer;
+    }
+}
