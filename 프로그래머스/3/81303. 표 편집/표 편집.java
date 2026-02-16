156636cf7071ceb410b2b2c41da342dc359ef2e4
ttangta <kimjh6836@gmail.com>
2026-02-16 17:34:40 +0900

    [level 3] Title: 표 편집, Time: 242.05 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,34 +1,26 @@
 import java.util.*;
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-
-        // 삭제된 행의 인덱스를 저장하는 스택
-        Stack<Integer> deleted = new Stack<>();
-
-        // 각 행을 기준으로 연산에 따른 위치를 표시하기 위한 배열
-        int[] up = new int[n + 2];
-        int[] down = new int[n + 2];
-
+        int[] up = new int[n+2];
+        int[] down = new int[n+2];
         for(int i=0; i<n+2; i++){
             up[i] = i-1;
             down[i] = i+1;
         }
-
-        // 현재 위치를 나타내는 인덱스 : 기존 테이블의 첫 번째 행 - 임시 공간이 생긴 새로운 테이블의 두 번째에 해당하기 때문에 초기 위치에는 +1
         k++;
 
-        // cmd 배열의 요소를 하나씩 처리
+        Stack<Integer> stack = new Stack<>();
         for(String c : cmd){
             if(c.startsWith("C")){
-                deleted.push(k);
+                stack.push(k);
                 up[down[k]] = up[k];
                 down[up[k]] = down[k];
                 k = n < down[k] ? up[k] : down[k];
             }
             else if(c.startsWith("Z")){
-                int restore = deleted.pop();
-                down[up[restore]] = restore;
+                int restore = stack.pop();
                 up[down[restore]] = restore;
+                down[up[restore]] = restore;
             }
             else{
                 String[] s = c.split(" ");
@@ -38,10 +30,10 @@ class Solution {
                 }
             }
         }
-        char[] answer = new char[n];
-        Arrays.fill(answer, 'O');
 
-        for(int i : deleted){
+        char[]answer = new char[n];
+        Arrays.fill(answer, 'O');
+        for(int i : stack){
             answer[i-1] = 'X';
         }
         return new String(answer);
