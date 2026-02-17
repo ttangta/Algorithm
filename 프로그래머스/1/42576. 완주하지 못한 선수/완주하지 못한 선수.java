60902a98099a03600a4ffbdc7265ee9f2f161ef4
ttangta <kimjh6836@gmail.com>
2026-02-17 18:30:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 99.57 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,17 @@
 import java.util.*;
+import java.util.Set;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-
-        for(String x : participant)hashMap.put(x, hashMap.getOrDefault(x, 0)+1);
-
-        for(String x : completion){
-            hashMap.put(x, hashMap.get(x)-1);
-            if(hashMap.get(x)==0)hashMap.remove(x);
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String s : participant)map.put(s, map.getOrDefault(s, 0)+1);
+        
+        for(String s : completion){
+            map.put(s, map.get(s)-1);
+            if(map.get(s)==0)map.remove(s);
         }
-
-        return  hashMap.keySet().iterator().next();
-
+        
+        return map.keySet().iterator().next();
     }
 }
