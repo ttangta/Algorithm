9799e60f6e012e8811f6d69f39fded1a26e6f4fb
ttangta <kimjh6836@gmail.com>
2026-02-19 14:39:50 +0900

    [level 2] Title: 할인 행사, Time: 60.23 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,19 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String,Integer> goal = new HashMap<>();
+        
+        HashMap<String, Integer> goal = new HashMap<>();
         
         for(int i=0; i<want.length; i++){
             goal.put(want[i], number[i]);
         }
         
-        for(int i=0; i<discount.length-9; i++){
-            HashMap<String,Integer> result = new HashMap<>();
+        for(int i=0; i<(discount.length-9); i++){
+            HashMap<String, Integer> result = new HashMap<>();
             for(int j=i; j<=(i+9); j++){
-                result.put(discount[j], result.getOrDefault(discount[j],0)+1);
+                result.put(discount[j], result.getOrDefault(discount[j], 0)+1);
             }
             if(goal.equals(result))answer++;
         }
+        
         
         return answer;
     }
