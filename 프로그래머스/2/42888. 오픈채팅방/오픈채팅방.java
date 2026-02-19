d5b41bca0394e5a7d23ead2f4179acc892a6b4aa
ttangta <kimjh6836@gmail.com>
2026-02-19 15:19:36 +0900

    [level 2] Title: 오픈채팅방, Time: 150.39 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,31 +1,27 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
-        HashMap<String, String> hashMap = new HashMap<>();
-
-        int n = record.length;
-
-        // 정답 문자열을 담을 문자열 배열
-        ArrayList<String> answer = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String cmd = null, uid = null, nickName = null;
-            String[] arr = record[i].split(" ");
-            cmd = arr[0];
-            uid = arr[1];
-            if(arr.length == 3)nickName = arr[2];
-
-            if(cmd.equals("Enter") || cmd.equals("Change")){
-                hashMap.put(uid, nickName);
+        HashMap<String, String> info = new HashMap<>();
+        
+        for(String s : record){
+            if(s.startsWith("Enter") || s.startsWith("Change")){
+                String[] arr = s.split(" ");
+                info.put(arr[1], arr[2]);
             }
         }
-
-        for(int i=0; i<n; i++){
-            String[] arr = record[i].split(" ");
-            if(arr[0].equals("Enter"))answer.add(hashMap.get(arr[1]) + "님이 들어왔습니다.");
-            else if(arr[0].equals("Leave"))answer.add(hashMap.get(arr[1]) + "님이 나갔습니다.");
-            else continue;
+        
+        ArrayList<String> answer = new ArrayList<>();
+        for(String s : record){
+            if(s.startsWith("Enter")){
+                String[] arr = s.split(" ");
+                answer.add(info.get(arr[1]) + "님이 들어왔습니다.");
+            }
+            else if(s.startsWith("Leave")){
+                String[] arr = s.split(" ");
+                answer.add(info.get(arr[1]) + "님이 나갔습니다.");
+            }
         }
-
-        return answer.stream().toArray(String[]::new);
+        
+        return answer.toArray(String[]::new);
     }
 }
