4cb4619fb4e2765ca4544aaeb531f09f2658d164
ttangta <kimjh6836@gmail.com>
2026-02-20 14:19:15 +0900

    [level 1] Title: 신고 결과 받기, Time: 192.18 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,44 +2,42 @@ import java.util.*;
 import java.util.stream.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        // 각 id가 받은 경고 횟수 HashMap
-        LinkedHashMap<String, Integer> totalWarnings = new LinkedHashMap<>();
+        HashMap<String, Integer> status = new HashMap<>();
+        HashMap<String, HashSet<String>> history = new HashMap<>();
+        HashMap<String, Integer> result = new HashMap<>();
+        
         for(String s : id_list){
-            totalWarnings.put(s, 0);
+            status.put(s, 0);
+            history.put(s, new HashSet<>());
+            result.put(s, 0);
         }
-
-        // 신고자-대상자 이력을 저장하는 HashMap 객체 생성 (신고이력이 없어도 아이디로 키는 존재해야함)
-        HashMap<String, HashSet<String>> reportHistory = new HashMap<>();
-        for(String s : id_list)reportHistory.put(s, new HashSet<>());
+        
         for(String s : report){
             String[] arr = s.split(" ");
             String from = arr[0];
             String by = arr[1];
-            reportHistory.get(from).add(by);
+            history.get(from).add(by);
         }
-
-        // reportHistory의 값을 totalWarnings의 키로 사용하여 해당 키의 값 1증가
+        
         for(String s : id_list){
-            HashSet<String> set = reportHistory.get(s);
-            for(String x : set){
-                totalWarnings.put(x, totalWarnings.get(x) + 1);
-            }
+            HashSet<String> target = history.get(s);
+            target.forEach(t -> {
+               status.put(t, status.get(t)+1); 
+            });
         }
-
-        // k이상 누적 경고를 받은 사용자
-        ArrayList<String> limitUser = totalWarnings.entrySet().stream().filter(entry -> entry.getValue() >= k).map(Map.Entry::getKey).collect(Collectors.toCollection(ArrayList::new));
-
-        LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
-        for(String s : id_list)result.put(s, 0);
-
-        for(String s : id_list){
-            int cnt = 0;
-            for(int i=0; i<limitUser.size(); i++){
-                if(reportHistory.get(s).contains(limitUser.get(i)))cnt++;
+        
+        String[] limited = status.entrySet().stream().filter(entry -> entry.getValue()>=k).map(Map.Entry::getKey).collect(Collectors.toCollection(ArrayList::new)).toArray(String[]::new);
+        
+        int[] answer = new int[id_list.length];
+        for(int i=0; i<id_list.length; i++){
+            String key = id_list[i];
+            for(String s : limited){
+                if(history.get(key).contains(s))result.put(key, result.get(key)+1);
             }
-            result.put(s, cnt);
+            
+            answer[i] = result.get(key);
         }
-
-        return result.entrySet().stream().map(Map.Entry::getValue).mapToInt(Integer::intValue).toArray();
-    }
+        
+        return answer;  
+    } 
 }
