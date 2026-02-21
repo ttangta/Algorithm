fbfc28cd6c6e6d372dff76d7807ac2534a81427e
ttangta <kimjh6836@gmail.com>
2026-02-21 13:58:07 +0900

    [level 3] Title: 베스트앨범, Time: 9.62 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,33 +3,23 @@ import java.util.stream.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         HashMap<String, Integer> total = new HashMap<>();
-        for(int i=0; i<genres.length; i++){
-            String genre = genres[i];
-            int play = plays[i];
-            total.put(genre, total.getOrDefault(genre, 0)+play);
-        }
-
         HashMap<String, ArrayList<int[]>> detail = new HashMap<>();
+        
         for(int i=0; i<genres.length; i++){
-            String genre = genres[i];
-            int play = plays[i];
-            if(!detail.containsKey(genre))detail.put(genre, new ArrayList<>());
-            detail.get(genre).add(new int[]{i, play});
+            total.put(genres[i], total.getOrDefault(genres[i], 0) + plays[i]);
+            if(!detail.containsKey(genres[i]))detail.put(genres[i], new ArrayList<>());
+            detail.get(genres[i]).add(new int[]{i, plays[i]});
         }
-
-        /*
-        Stream<Map.Entry<String,Integer>> sg = total.entrySet().stream().sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()));
-        sg.forEach(s -> System.out.println(s.getKey() + " : " + s.getValue()));
-        */
-
-        Stream<String> sortedGenre = total.entrySet().stream().sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue())).map(Map.Entry::getKey);
-
+        
         ArrayList<Integer> answer = new ArrayList<>();
-        sortedGenre.forEach(s -> {
-            Stream<int[]> sortedSong = detail.get(s).stream().sorted((o1, o2) -> Integer.compare(o2[1], o1[1])).limit(2);
-            sortedSong.forEach(song -> answer.add(song[0]));
+        
+        Stream<String> gStream = total.entrySet().stream().sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue())).map(Map.Entry::getKey);
+        
+        gStream.forEach(g -> {
+            Stream<int[]> sStream = detail.get(g).stream().sorted((o1, o2) -> Integer.compare(o2[1], o1[1])).limit(2);
+            sStream.forEach(s -> answer.add(s[0]));
         });
-
+        
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
