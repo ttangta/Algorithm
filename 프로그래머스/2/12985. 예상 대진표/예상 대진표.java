628f02481d3668fd1a8cd39e411f15dd3ca61f42
ttangta <kimjh6836@gmail.com>
2026-02-21 21:17:55 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,13 +2,12 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer;
-
-        for(answer=0; a != b; answer++){
+        int answer = 0;
+        while(a != b){
+            answer++;
             a = (a+1)/2;
             b = (b+1)/2;
         }
-
         return answer;
     }
 }
