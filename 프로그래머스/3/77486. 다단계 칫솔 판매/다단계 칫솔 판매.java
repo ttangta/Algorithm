0c310e57fab6779216b120dfe91dbb42a5ebdc5f
ttangta <kimjh6836@gmail.com>
2026-02-22 16:37:02 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 66.61 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,30 +1,24 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        // 1. 판매원 - 추천인 형태의 HashMap 객체 생성
-        HashMap<String, String> parent = new HashMap<>();
+        HashMap<String, String> relation = new HashMap<>();
         for(int i=0; i<enroll.length; i++){
-            parent.put(enroll[i], referral[i]);
+            relation.put(enroll[i], referral[i]);
         }
         
-        // 2. 판매원 - 수익 형태의 HashMap 객체 생성
         HashMap<String, Integer> total = new HashMap<>();
-        
-        // 3. seller[i]의 실적에 대하여 해당 판매원의 추천인 관계를 올라가며 수수료 작업 후 total의 seller[i]의 값 저장
         for(int i=0; i<seller.length; i++){
-            int money = amount[i] * 100;
             String curName = seller[i];
-            
+            int money = amount[i]*100;
             while(money > 0 && !curName.equals("-")){
                 total.put(curName, total.getOrDefault(curName, 0) + money - (money/10));
-                curName = parent.get(curName);
+                curName = relation.get(curName);
                 money /= 10;
             }
         }
-        int[] answer = new int[enroll.length];
-        for(int i=0; i<enroll.length; i++){
-            answer[i] = total.getOrDefault(enroll[i], 0);
-        }
-        return answer;
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(String s : enroll)answer.add(total.getOrDefault(s, 0));
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
