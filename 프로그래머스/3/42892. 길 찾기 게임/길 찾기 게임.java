c953a394cb12911a6dedb7275aef7f6ebc368323
ttangta <kimjh6836@gmail.com>
2026-02-23 20:31:37 +0900

    [level 3] Title: 길 찾기 게임, Time: 37.78 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class Solution {
-    // Node 클래스
-    private static class Node{
+    public static class Node{
         int num;
         int x;
         int y;
@@ -13,74 +12,64 @@ class Solution {
             this.y = y;
         }
     }
-    // 트리 생성 메서드
-    private static Node makeBT(int[][] nodeinfo){
-        // nodeInfo 이차원 배열의 행만큼 노드 생성
-        Node[] nodes = new Node[nodeinfo.length];
-        // nodeInfo 행번호+1 값이 각 노드의 이름
+    private static Node[] nodes;
+    public int[][] solution(int[][] nodeinfo) {
+        nodes = new Node[nodeinfo.length];
+        
         for(int i=0; i<nodeinfo.length; i++){
             nodes[i] = new Node(i+1, nodeinfo[i][0], nodeinfo[i][1]);
         }
         
-        // Node의 y값 기준 내림차순 정렬, y값이 같을 경우 x값 기준 오름차순
-        Arrays.sort(nodes, (o1, o2) -> {
-           if(o1.y == o2.y){
-               return Integer.compare(o1.x, o2.x);
-           }
-           return Integer.compare(o2.y, o1.y);
-        });
+        nodes = Arrays.stream(nodes).sorted((o1, o2) -> {
+            if(o1.y == o2.y)return Integer.compare(o1.x, o2.x);
+            return Integer.compare(o2.y, o1.y);
+        }).toArray(Node[]::new);
         
         Node root = nodes[0];
-        
         for(int i=1; i<nodes.length; i++){
-            Node parent = root;
+            Node p = root;
             while(true){
-                if(nodes[i].x < parent.x){
-                    if(parent.left == null){
-                        parent.left = nodes[i];
+                if(p.x > nodes[i].x){
+                    if(p.left == null){
+                        p.left = nodes[i];
                         break;
                     }else{
-                        parent = parent.left;
+                        p = p.left;
                     }
                 }
                 else{
-                    if(parent.right == null){
-                        parent.right = nodes[i];
+                    if(p.right == null){
+                        p.right = nodes[i];
                         break;
                     }else{
-                        parent = parent.right;
+                        p = p.right;
                     }
                 }
             }
         }
-        return nodes[0];
-    }
-    
-    private static void preOrder(Node curr, ArrayList<Integer>answer){
-        if(curr == null)return;
-        answer.add(curr.num);
-        preOrder(curr.left, answer);
-        preOrder(curr.right, answer);
-    }
-    
-    private static void postOrder(Node curr, ArrayList<Integer>answer){
-        if(curr == null)return;
-        postOrder(curr.left, answer);
-        postOrder(curr.right, answer);
-        answer.add(curr.num);
-    }
-    
-    public int[][] solution(int[][] nodeinfo) {
-        Node root = makeBT(nodeinfo);
-        ArrayList<Integer> preOrderList = new ArrayList<>();
-        preOrder(root, preOrderList);
-        ArrayList<Integer> postOrderList = new ArrayList<>();
-        postOrder(root,postOrderList);
+        ArrayList<Integer> pre = new ArrayList<>();
+        preOrder(root, pre);
+        ArrayList<Integer> post = new ArrayList<>();
+        postOrder(root, post);
         
         int[][] answer = new int[2][nodeinfo.length];
-        answer[0] = preOrderList.stream().mapToInt(Integer::intValue).toArray();
-        answer[1] = postOrderList.stream().mapToInt(Integer::intValue).toArray();
+        answer[0] = pre.stream().mapToInt(Integer::intValue).toArray();
+        answer[1] = post.stream().mapToInt(Integer::intValue).toArray();
         
         return answer;
+    
+    }
+    private static void preOrder(Node node, ArrayList<Integer> pre){
+        if(node == null)return;
+        pre.add(node.num);
+        preOrder(node.left, pre);
+        preOrder(node.right, pre);
+    }
+    
+    private static void postOrder(Node node, ArrayList<Integer> post){
+        if(node == null)return;
+        postOrder(node.left, post);
+        postOrder(node.right, post);
+        post.add(node.num);
     }
 }
