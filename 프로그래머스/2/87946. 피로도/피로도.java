64ac15d5a08c720c19b816133e3af3d4042275d4
ttangta <kimjh6836@gmail.com>
2026-03-15 09:14:47 +0900

    [level 2] Title: 피로도, Time: 2.69 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,11 +1,11 @@
 class Solution {
+    private static boolean[]visited;
     private static int answer;
     private static int[][] Dungeons;
-    private static boolean[] visited;
     public int solution(int k, int[][] dungeons) {
-        answer = 0;
         Dungeons = dungeons;
-        visited = new boolean[dungeons.length];
+        visited = new boolean[Dungeons.length];
+        answer = 0;
         backTrack(k, 0);
         return answer;
     }
@@ -13,8 +13,8 @@ class Solution {
         for(int i=0; i<Dungeons.length; i++){
             if(!visited[i] && k >= Dungeons[i][0]){
                 visited[i] = true;
-                backTrack(k - Dungeons[i][1], cnt + 1);
-                answer = Math.max(answer, cnt + 1);
+                backTrack(k - Dungeons[i][1], cnt+1);
+                answer = Math.max(answer, cnt+1);
                 visited[i] = false;
             }
         }
