bfa93dbea71795d3eafe70ab34548fc7bd1a8be7
ttangta <kimjh6836@gmail.com>
2026-03-18 09:41:59 +0900

    [level 2] Title: N-Queen, Time: 81.55 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,27 +1,29 @@
 class Solution {
     private static boolean[] width;
-    private static boolean[] ldDiagonal;
-    private static boolean[] rdDiagonal;
+    private static boolean[] lDiagonal;
+    private static boolean[] rDiagonal;
     private static int N;
     public int solution(int n) {
-        N = n;
         width = new boolean[n];
-        ldDiagonal = new boolean[n*2];
-        rdDiagonal = new boolean[n*2];
-        return backTrack(0);
+        lDiagonal = new boolean[n*2];
+        rDiagonal = new boolean[n*2];
+        N = n;
+        int answer = backTrack(0);
+        return answer;
     }
-    private int backTrack(int y){
-        int answer = 0;
-        if(y == N)answer++;
+    private static int backTrack(int y){
+        int ans = 0;
+        if(y == N){
+            ans++;
+        }
         else{
             for(int i=0; i<N; i++){
-                if(width[i] || ldDiagonal[i+y] || rdDiagonal[i-y+N])continue;
-                width[i] = ldDiagonal[i+y] = rdDiagonal[i-y+N] = true;
-                answer += backTrack(y+1);
-                width[i] = ldDiagonal[i+y] = rdDiagonal[i-y+N] = false;
+                if(width[i] || lDiagonal[i+y] || rDiagonal[i-y+N])continue;
+                width[i] = lDiagonal[i+y] = rDiagonal[i-y+N] = true;
+                ans += backTrack(y+1);
+                width[i] = lDiagonal[i+y] = rDiagonal[i-y+N] = false;
             }
         }
-        
-        return answer;
+        return ans;
     }
 }
