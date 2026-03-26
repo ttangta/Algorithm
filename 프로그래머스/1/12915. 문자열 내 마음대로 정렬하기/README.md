b7bfa73b85139fc409c9390f174abb024de42c0c
ttangta <kimjh6836@gmail.com>
2026-03-26 15:51:42 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.34 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915#) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 1.14 ms
+메모리: 74.3 MB, 시간: 1.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 26일 13:50:17
+2026년 03월 26일 15:51:37
 
 ### 문제 설명
 
--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,7 +1,17 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, (o1, o2) -> o1.charAt(n) == o2.charAt(n)? o1.compareTo(o2) : Character.compare(o1.charAt(n), o2.charAt(n)));
-        return strings;
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
+            if(a.charAt(n) == b.charAt(n))return a.compareTo(b);
+            else return Integer.compare(a.charAt(n), b.charAt(n));
+        });
+            
+        for(String s : strings){
+            pq.offer(s);
+        }
+        
+        String[] answer = new String[strings.length];
+        for(int i=0; i<strings.length; i++)answer[i] = pq.poll();
+        return answer;
     }
 }
