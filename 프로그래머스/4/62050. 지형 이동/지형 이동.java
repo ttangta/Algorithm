ee1520683e9357f3c32c56e0c0606a733f263d4d
ttangta <kimjh6836@gmail.com>
2026-03-28 11:57:23 +0900

    [level 4] Title: 지형 이동, Time: 157.11 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.java
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.java
@@ -1,11 +1,9 @@
 import java.util.*;
 class Solution {
-    private static final int[][] MV= {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static final int[][] MV = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     private static int N;
     private static boolean[][] visited;
-    private static boolean isValid(int i, int j){
-        return 0<= i && i<N && 0<=j && j<N;
-    }
+    
     private static class Node{
         int i, j, cost;
         public Node(int i, int j, int cost){
@@ -14,21 +12,23 @@ class Solution {
             this.cost = cost;
         }
     }
+    
+    private static boolean isValid(int i, int j){
+        return 0 <= i && i < N && 0 <= j && j < N;
+    }
+    
     public int solution(int[][] land, int height) {
+        int answer = 0;
         N = land.length;
         visited = new boolean[N][N];
-        
-        int answer = 0;
         
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
         pq.add(new Node(0, 0, 0));
         
         while(!pq.isEmpty()){
             Node now = pq.poll();
-            
             if(visited[now.i][now.j])continue;
             visited[now.i][now.j] = true;
-            
             answer += now.cost;
             
             for(int i=0; i<4; i++){
@@ -36,13 +36,11 @@ class Solution {
                 int nj = now.j + MV[i][1];
                 if(isValid(ni, nj)){
                     int tempCost = Math.abs(land[now.i][now.j] - land[ni][nj]);
-                    int newCost = tempCost > height ? tempCost : 0;
+                    int newCost = tempCost>height?tempCost:0;
                     pq.add(new Node(ni, nj, newCost));
                 }
             }
         }
-        
-        
         return answer;
     }
 }
