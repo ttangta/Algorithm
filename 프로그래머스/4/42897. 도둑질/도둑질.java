a29cb01f3685941646aa40cf7f545b71461909c3
ttangta <kimjh6836@gmail.com>
2026-04-06 15:31:08 +0900

    [level 4] Title: 도둑질, Time: 27.30 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,19 +1,28 @@
 class Solution {
     public int solution(int[] money) {
+        // 집의 갯수
         int n = money.length;
+
+        // 0 접근한 경우 -> (N-1) 접근 불가
         int[] dp1 = new int[n];
+
+        // 0번 시작 dp1 초기화 (0, 1번값 초기화 가능)
         dp1[0] = money[0];
+        // 0번 시작 시 1번 접근 불가
         dp1[1] = dp1[0];
+
+        // 범위(2 ~ (N-1)이전)
         for(int i=2; i<n-1; i++){
-            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + money[i]);   
+            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + money[i]);
         }
-        
+
+        // 0번 부터 시작하지 않는 경우 -> (N-1) 접근 가능
         int[] dp2 = new int[n];
         dp2[1] = money[1];
         for(int i=2; i<n; i++){
             dp2[i] = Math.max(dp2[i-1], dp2[i-2] + money[i]);
         }
-        
+
         return Math.max(dp1[n-2], dp2[n-1]);
-    } 
+    }
 }
