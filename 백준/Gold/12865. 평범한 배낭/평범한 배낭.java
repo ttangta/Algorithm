f9f59b6ab9c4bac3cc3cf3ae7dc628fadb1d5929
ttangta <kimjh6836@gmail.com>
2026-04-11 20:51:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53756 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,30 +1,31 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main{
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        
         int[][] info = new int[n+1][2];
         for(int i=1; i<=n; i++){
             st = new StringTokenizer(br.readLine());
             info[i][0] = Integer.parseInt(st.nextToken());
             info[i][1] = Integer.parseInt(st.nextToken());
         }
+        System.out.println(solution(n, k, info));
+    }
+    private static int solution(int n, int k, int[][] info){
         int[][] dp = new int[n+1][k+1];
         for(int i=1; i<=n; i++){
             int weight = info[i][0];
             int value = info[i][1];
             for(int j=0; j<=k; j++){
-                if(j >= weight){
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight] + value);
-                }
+                if(j >= weight)dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight] + value);
                 else dp[i][j] = dp[i-1][j];
             }
         }
-        System.out.println(dp[n][k]);
-    } 
-   
+        return dp[n][k];
+    }
+    
 }
