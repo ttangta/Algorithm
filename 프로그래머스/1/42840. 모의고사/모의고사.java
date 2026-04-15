7ae898dbe7477364ccee4f4cfa15e844b8ea5dd1
ttangta <kimjh6836@gmail.com>
2026-04-15 11:55:37 +0900

    [level 1] Title: 모의고사, Time: 6.95 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,25 +1,22 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    private static int[][] pattern = {
-        {1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3,3,1,1,2,2,4,4,5,5}
-    };
+    private static final int[][] pattern = {{1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3,3,1,1,2,2,4,4,5,5}};
     public int[] solution(int[] answers) {
-        int[] scores = new int[pattern.length];
-        
+        int[] score = new int[pattern.length];
         for(int i=0; i<answers.length; i++){
-            for(int j=0; j<scores.length; j++){
-                if(answers[i] == pattern[j][i%pattern[j].length])scores[j]++;
+            int answer = answers[i];
+            for(int j=0; j<pattern.length; j++){
+                int studentAnaswer = pattern[j][i % (pattern[j].length)];
+                if(answer == studentAnaswer)score[j]++;
             }
         }
-        
-        int max = Arrays.stream(scores).max().getAsInt();
-        
+        int max = Arrays.stream(score).max().getAsInt();
         ArrayList<Integer> answer = new ArrayList<>();
-        
-        for(int i=0; i<scores.length; i++){
-            if(scores[i] == max)answer.add(i+1);
+        for(int i=0; i<score.length; i++){
+            if(max == score[i])answer.add(i+1);
         }
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().sorted(Comparator.naturalOrder()).mapToInt(Integer::intValue).toArray();
     }
 }
