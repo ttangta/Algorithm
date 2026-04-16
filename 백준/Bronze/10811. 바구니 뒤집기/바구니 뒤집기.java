3e9b1dd8a6e7b21f3f870701d3679d69fb938093
ttangta <kimjh6836@gmail.com>
2026-04-16 14:40:27 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String[] command = new String[m];
+        for(int i=0; i<m; i++)command[i] = br.readLine();
+        System.out.println(solution(n, command));
+    }
+    private static String solution(int n, String[] command){
+        int[] arr = new int[n+1];
+        for(int i=0; i<=n; i++)arr[i] = i;
+        StringBuilder sb = new StringBuilder();
+        for(String s : command){
+            String[] cmd = s.split(" ");
+            int i = Integer.parseInt(cmd[0]);
+            int j = Integer.parseInt(cmd[1]);
+            while(i < j){
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+        for(int i=1; i<=n; i++)sb.append(arr[i]).append(" ");
+        return sb.toString().trim();
+    }
+}
