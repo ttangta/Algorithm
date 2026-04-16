517c6cad790df033f6cf03cf3acab7f0ec2a13cf
ttangta <kimjh6836@gmail.com>
2026-04-16 09:13:36 +0900

    [Bronze V] Title: 개수 세기, Time: 112 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -9,9 +9,6 @@ public class Main{
         System.out.println(solution(n, numbers, v));
     }
     private static int solution(int n, String numbers, int v){
-        int answer = 0;
-        int[] arr = Arrays.stream(numbers.split(" ")).mapToInt(Integer::parseInt).toArray();
-        for(int x : arr)if(x == v)answer++;
-        return answer;
+        return (int)Arrays.stream(numbers.split(" ")).mapToInt(Integer::parseInt).filter(num -> num==v).count();
     }
 }
