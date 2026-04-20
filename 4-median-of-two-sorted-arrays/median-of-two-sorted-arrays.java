5ff286ff0122ee3c8d9585a06e62932a15da0137
ttangta <kimjh6836@gmail.com>
2026-04-20 20:31:40 +0900

    Time: 1 ms (100.00%) | Memory: 49 MB (36.52%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,15 +1,33 @@
-import java.util.*;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] arr = new int[nums1.length + nums2.length];
-        for(int i=0; i<nums1.length; i++)arr[i] = nums1[i];
-        for(int i=0; i<nums2.length; i++)arr[nums1.length + i] = nums2[i];
-        Arrays.sort(arr);
-        int count = arr.length%2==0?2:1;
-        int mid = arr.length/2;
-        int startIdx = arr.length%2==0?mid-1:mid;
-        int sum = 0;
-        for(int i=0; i<count; i++)sum+=arr[startIdx + i];
-        return (double)sum / count;
+        if(nums1.length > nums2.length){
+            int[] temps = nums1.clone();
+            nums1 = nums2;
+            nums2 = temps;
+        }
+        int m = nums1.length;
+        int n = nums2.length;
+        
+        int left = 0, right = m;
+        while(left <= right){
+            int partition1 = (left + right) / 2;
+            int partition2 = (m + n + 1) / 2 - partition1;
+
+            int maxLeft1 = partition1==0?Integer.MIN_VALUE:nums1[partition1-1];
+            int minRight1 = partition1==m?Integer.MAX_VALUE:nums1[partition1];
+
+            int maxLeft2 = partition2==0?Integer.MIN_VALUE:nums2[partition2-1];
+            int minRight2 = partition2==n?Integer.MAX_VALUE:nums2[partition2];
+
+            if(maxLeft1 <= minRight2 && maxLeft2 <= minRight1){
+                if((m+n)%2==0)return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2))/2.0;
+                else return Math.max(maxLeft1, maxLeft2);
+            }else if(maxLeft1 > minRight2){
+                right = partition1 - 1;
+            }else{
+                left = partition1 + 1;
+            }
+        }
+        return 0.;
     }
 }
