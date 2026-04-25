eed73c5e7fe8d9f87f406fbcc033ba3e3701d356
ttangta <kimjh6836@gmail.com>
2026-04-25 15:21:39 +0900

    Time: 5 ms (83.11%) | Memory: 77.6 MB (13.94%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -3,8 +3,8 @@ class Solution {
         int left = 0, right = height.length-1, answer = 0;
         while(left < right){
             answer = Math.max(answer, (right-left) * Math.min(height[left], height[right]));
-            if(height[right] < height[left])right--;
-            else left++;
+            if(height[left] <= height[right])left++;
+            else right--;
         }
 
         return answer;
