1d1d789c1a24895aa4a9df92c5bf337330465462
ttangta <kimjh6836@gmail.com>
2026-04-25 17:14:13 +0900

    Time: 3 ms (99.92%) | Memory: 46.4 MB (63.08%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -3,14 +3,10 @@ class Solution {
         int[] n = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] roman = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         StringBuilder sb = new StringBuilder();
-        while(num > 0){
-            for(int i=0; i<n.length; i++){
-                if(num - n[i] >= 0){
-                    sb.append(roman[i]);
-                    num -= n[i];
-                    break;
-                }
-                else continue;
+        for(int i=0; i<n.length; i++){
+            while(num >= n[i]){
+                sb.append(roman[i]);
+                num -= n[i];
             }
         }
         return sb.toString();
