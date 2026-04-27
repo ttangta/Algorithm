3554f217c41d17728bd34ec1ed7693eeedb33ae0
ttangta <kimjh6836@gmail.com>
2026-04-28 08:39:25 +0900

    Time: 4 ms (49.93%) | Memory: 46.7 MB (89.52%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,22 @@
 import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
+        // nums의 배열크기만큼 반복하면서 nums[i] 값을 키 i 즉, 인덱스를 값으로 지정 "nums[i]:i" 형태
         for(int i=0; i<nums.length; i++)map.put(nums[i], i);
 
-        int[] answer = new int[2];
 
+        // nums의 각 요소에 접근하면서 taget에서 nums[i]의 요소의 값을 뺀 값 계산 이를 other 이라고 지칭함
         for(int i=0; i<nums.length; i++){
-            if(map.containsKey(target-nums[i]) && i != map.get(target-nums[i])){
+            int other = target - nums[i];
+            // 만약 other의 값이 map의 키에 존재한다면 현재 nums[i] 번째 인덱스와 map의 other의 값에 해당하는 nums의 인덱스 값의 합으로 target 값 도출 가능
+            if(map.containsKey(other) && map.get(other) != i){
                 answer[0] = i;
-                answer[1] = map.get(target-nums[i]);
-            }
+                answer[1] = map.get(other);
+                return answer;
+            }   
         }
-        return answer;
-    }   
+        return null;
+    }
 }
