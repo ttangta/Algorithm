2c3e953d67b47a9efb969ee3682d25b2157063ee
ttangta <kimjh6836@gmail.com>
2026-04-28 09:50:50 +0900

    Time: 1 ms (100.00%) | Memory: 46.4 MB (58.31%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,22 +10,24 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode initNode = new ListNode(0);
-        ListNode current = initNode;
-        int carry = 0;
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        int mod = 0;
 
-        while(l1 != null || l2 != null || carry != 0){
-            int n1 = l1 != null? l1.val : 0;
-            int n2 = l2 != null? l2.val : 0;
-            int sum = n1 + n2 + carry;
+        // l1이 null이 아니거나, l2가 null이 아니거나, mod가 0이 아닌동안
+        // 즉, 종료조건은 l1==null l2==null, mod==0 모든 조건이 부합할때 종료 
+        while(l1 != null || l2 != null || mod != 0){
+            int n1 = l1!=null?l1.val:0;
+            int n2 = l2!=null?l2.val:0;
+            int sum = n1 + n2 + mod;
 
-            carry = sum/10;
             current.next = new ListNode(sum%10);
-            current = current.next;
+            mod = sum/10;
 
-            if(l1 != null)l1 = l1.next;
-            if(l2 != null)l2 = l2.next;
+            l1 = l1!=null?l1.next:null;
+            l2 = l2!=null?l2.next:null;
+            current = current.next; 
         }
-        return initNode.next;
+        return dummy.next;
     }
 }
