36281acf811798a797f66166d3ddb8d65656383d
ttangta <kimjh6836@gmail.com>
2026-04-30 17:04:29 +0900

    Time: 0 ms (100.00%) | Memory: 43.5 MB (57.83%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,32 +10,20 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // 원본 head는 그대로 반환으로 사용해야하기 떄문에 복사본 생성
-        ListNode clone = head;
-        int idx = 0;
-        // 1. 리스트의 크기 측정
-        while(clone != null){
-            idx++;
-            clone = clone.next;
+        ListNode clone = new ListNode(0, head);
+        ListNode fast = clone;
+        ListNode slow = clone;
+
+        for(int i=0; i<n; i++){
+            fast = fast.next;
         }
 
-        // 2. 뒤에서 n번째 노드를 제거하기 위해서는 뒤에서 n-1번 노드의 수정 필요 -> 수정해야하는 노드의 위치를 나타내는 변수
-        int target = idx - n;
-
-        // 만약 0번째 노드의 수정이 필요한 경우 단순 매개변수로 전달받은 head의 next 부터 그대로 출력하면 됨
-        if(target == 0)return head.next;
-        
-        // target번 노드를 수정하기 위해 idx와, clone을 다시 최초의 값으로 초기화
-        idx = 0;
-        clone = head;
-        while(clone != null){
-            idx++;
-            if(idx == target){
-                clone.next = clone.next.next;
-                break;
-            }
-            clone = clone.next;
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
         }
-        return head;
+        slow.next = slow.next.next;
+
+        return clone.next;
     }
 }
