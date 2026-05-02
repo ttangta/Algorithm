850034bde9c7d8aa11ba9cce430965babc25c98a
ttangta <kimjh6836@gmail.com>
2026-05-02 15:48:07 +0900

    Time: 0 ms (100.00%) | Memory: 43.9 MB (98.37%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,25 +1,26 @@
 import java.util.*;
 class Solution {
+    private static List<String> answer;
     public List<String> generateParenthesis(int n) {
-        List<String> answer = new ArrayList<>();
-
-        backTrack(0, 0, n, new StringBuilder(), answer);
-
+        answer = new ArrayList<>();
+        backTrack(0, 0, n, new StringBuilder());
         return answer;
     }
-    private static void backTrack(int open, int close, int n, StringBuilder sb, List<String> answer){
+    private static void backTrack(int open, int close, int n, StringBuilder sb){
         if(sb.length() == n*2){
             answer.add(sb.toString());
             return;
         }
+
         if(open < n){
             sb.append('(');
-            backTrack(open +1, close, n, sb, answer);
+            backTrack(open+1, close, n, sb);
             sb.deleteCharAt(sb.length()-1);
         }
+
         if(close < open){
             sb.append(')');
-            backTrack(open, close+1, n, sb, answer);
+            backTrack(open, close+1, n, sb);
             sb.deleteCharAt(sb.length()-1);
         }
     }
