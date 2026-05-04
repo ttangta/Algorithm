8732465fd6ebb8a01125afc5938f9dd500e01a8d
ttangta <kimjh6836@gmail.com>
2026-05-04 13:44:52 +0900

    Time: 15 ms (98.12%) | Memory: 45.4 MB (80.99%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,25 +1,28 @@
 import java.util.*;
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        // two pointer을 위한 정렬 처리
+        int close = Integer.MAX_VALUE;
+        int answer = Integer.MAX_VALUE;
+
+        // 투 포인터 사용을 위한 nums 배열 정렬
         Arrays.sort(nums);
 
-        // 최초 세 요소의 합은 단순 nums의 앞단 3요소의 합
-        int answer = nums[0] + nums[1] + nums[2];
-        // target 과 최초 세 요소의 합의 차
-        int dif = Math.abs(target-answer);
-
-        for(int p1 = 0; p1 < nums.length-2; p1++){
-            int p2 = p1+1;
-            int p3 = nums.length-1;
-            while(p2 < p3){
-                int sum = nums[p1] + nums[p2] + nums[p3];
-                if(dif > Math.abs(target-sum)){
-                    dif = Math.abs(target-sum);
+        // 세 개의 요소의 합을 구해야 하므로 이동을 하는 두 개의 포인터 외의 하나의 고정된 값 지정
+        for(int i=0; i<nums.length-2; i++){
+            int point = nums[i];
+            int left = i+1, right = nums.length-1;
+            while(left < right){
+                int sum = point + nums[left] + nums[right];
+                // 현재 계산된 합과 target의 차와 기존 근사값의 차를 비교하여 현재 sum에 대한 근사값이 더 작으면 갱신
+                if(close > Math.abs(sum-target)){
+                    close = Math.abs(sum-target);
                     answer = sum;
                 }
-                if(sum < target)p2++;
-                else if(sum > target)p3--;
+                // 만약 현재 sum의 값이 target 보다 작으면 작은 값을 가리키는 left 포인트를 이동 시켜 값을 증가 시킴
+                if(sum < target)left++;
+                // 만약 현재 sum의 값이 target 보다 크면 큰 값을 가리키는 right 포인트를 이동 시켜 값을 증가 시킴
+                else if(sum > target)right--;
+                // 만약 현재 sum의 값이 target과 정확히 일치하면 현재 sum 그 자체로 답이기 때문에 sum 그래도 반환
                 else return sum;
             }
         }
