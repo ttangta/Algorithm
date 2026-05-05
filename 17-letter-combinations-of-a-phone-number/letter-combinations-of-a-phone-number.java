bc90198dca8fe4514bb9ccb0df956bbe4c144848
ttangta <kimjh6836@gmail.com>
2026-05-05 09:45:15 +0900

    Time: 1 ms (54.89%) | Memory: 43.5 MB (66.25%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,35 +1,21 @@
-import java.util.*;
 class Solution {
-    private static HashMap<Character, char[]> map = new HashMap<>();
-    private static void mkMap(){
-        map = new HashMap<>();
-        map.put('2', new char[]{'a','b','c'});
-        map.put('3', new char[]{'d','e','f'});
-        map.put('4', new char[]{'g', 'h', 'i'});
-        map.put('5', new char[]{'j', 'k', 'l'});
-        map.put('6', new char[]{'m', 'n', 'o'});
-        map.put('7', new char[]{'p', 'q', 'r', 's'});
-        map.put('8', new char[]{'t', 'u', 'v'});
-        map.put('9', new char[]{'w', 'x', 'y', 'z'});
-    }
+    private static final String[] str = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    private static List<String> answer;
     public List<String> letterCombinations(String digits) {
-        List<String> answer = new ArrayList<>();
-        mkMap();
-        combinations(0, digits,"", answer);
-
+        answer = new ArrayList<>();
+        combinations(0, digits, new StringBuilder());
         return answer;
     }
-    private static void combinations(int idx, String digits, String current, List<String> answer){
-        if(digits.length() == current.length()){
-            answer.add(current);
+    private static void combinations(int idx, String digits, StringBuilder sb){
+        if(sb.length() == digits.length()){
+            answer.add(sb.toString());
             return;
         }
-
-        char key = digits.charAt(idx);
-        char[] values = map.get(key);
-        for(int i=0; i<values.length; i++){
-            combinations(idx+1, digits, current + values[i], answer);
+        int pn = Integer.parseInt(String.valueOf(digits.charAt(idx)));
+        for(int i=0; i<str[pn].length(); i++){
+            sb.append(str[pn].charAt(i));
+            combinations(idx+1, digits, sb);
+            sb.deleteCharAt(sb.length()-1);
         }
-
-    }  
+    }
 }
