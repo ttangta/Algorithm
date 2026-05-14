4ad02f1963264a7253a8ec84eedbb3e9e69dc8cb
ttangta <kimjh6836@gmail.com>
2026-05-14 19:42:58 +0900

    Time: 5 ms (83.13%) | Memory: 46.5 MB (41.56%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -4,20 +4,15 @@ class Solution {
         int answer = 0;
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);
-
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             if(c == '(')stack.push(i);
             else{
                 stack.pop();
-                if(stack.isEmpty()){
-                    stack.push(i);
-                }else{
-                    answer = Math.max(answer, i-stack.peek());
-                }
+                if(stack.isEmpty())stack.push(i);
+                else answer = Math.max(answer, i-stack.peek());
             }
         }
         return answer;
     }
-    
 }
