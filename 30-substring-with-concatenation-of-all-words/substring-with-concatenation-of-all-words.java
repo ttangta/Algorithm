6b847d380b3bab38d682071a79903ee9d5092148
ttangta <kimjh6836@gmail.com>
2026-05-14 09:02:43 +0900

    Time: 15 ms (12.04%) | Memory: 48.1 MB (5.06%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -2,42 +2,52 @@ import java.util.*;
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> answer = new ArrayList<>();
-        int wordCount = words.length;
-        int wordLen = words[0].length();
+        if(s == null || words == null || words.length == 0)return answer;
+        // 단어의 개수
+        int m = words.length;
+        // 단어 하나의 길이
+        int n = words[0].length();
+        int total = m*n;
 
-        Map<String, Integer> wordFreq = new HashMap<String, Integer>();
-        for(String word : words)wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
+        // words의 빈도수 맵
+        Map<String, Integer> wordMap = new HashMap<>();
+        for(String word : words)wordMap.put(word, wordMap.getOrDefault(word, 0)+1);
 
-        for(int offset=0; offset<wordLen; offset++){
+        // 해당 부분을 구현하지 못함
+        for(int offset = 0; offset < n; offset++){
+            Map<String, Integer> window = new HashMap<>();
             int left = offset;
-            int right = offset;
-            int matched = 0;
-            Map<String, Integer> windowFeq = new HashMap<>();
-            while(right + wordLen <= s.length()){
-                String rightWord = s.substring(right, right+wordLen);
-                right+=wordLen;
-
-                if(!wordFreq.containsKey(rightWord)){
-                    windowFeq.clear();
-                    matched = 0;
-                    left = right;
-                    continue;
+            // 빈도까지 일치하는 단어 수
+            int match = 0;
+            for(int right = offset; right + n <= s.length(); right+=n){
+                // 오른쪽 단어 추가
+                String word = s.substring(right, right+n);
+                if(wordMap.containsKey(word)){
+                    window.put(word, window.getOrDefault(word, 0)+1);
+                    if(window.get(word).equals(wordMap.get(word))){
+                        match++;
+                    }else if(window.get(word).equals(wordMap.get(word)+1)){
+                        match--;
+                    }
                 }
 
-                windowFeq.put(rightWord, windowFeq.getOrDefault(rightWord, 0) + 1);
-                matched++;
-
-                while(windowFeq.get(rightWord) > wordFreq.get(rightWord)){
-                    String leftWord = s.substring(left, left+wordLen);
-                    windowFeq.put(leftWord, windowFeq.get(leftWord)-1);
-                    if(windowFeq.get(leftWord) == 0)windowFeq.remove(leftWord);
-                    matched--;
-                    left += wordLen;
+                // 윈도우가 넘치면 왼쪽 단어 제거
+                if(right - left >= total){
+                    String leftWord = s.substring(left, left+n);
+                    if(wordMap.containsKey(leftWord)){
+                        if(window.get(leftWord).equals(wordMap.get(leftWord))){
+                            match--;
+                        }else if(window.get(leftWord).equals(wordMap.get(leftWord)+1)){
+                            match++;
+                        }
+                        window.put(leftWord, window.get(leftWord)-1);
+                    }
+                    left += n;
                 }
-                
-                if(matched == wordCount)answer.add(left);
+                if(match == wordMap.size())answer.add(left);
             }
         }
+
 
         return answer;
     }
